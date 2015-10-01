44ee31fc8a61aa9b73fd42ca55d7fd56589a0388
Valerie Mettler <valeriemettler@gmail.com>
2015-10-01 14:08:11 -0700

    update sanitation method

--- js/main2.js
+++ js/main2.js
@@ -1,1 +1,27 @@
-window.html_sanitize("hi");
+console.log("CL is working!!");
+var a = "hi";
+var b = "<h1>hi</h1>";
+var removeTags = function(html) {
+    var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+    var tagOrComment = new RegExp(
+        '<(?:'
+        // Comment body.
+        + '!--(?:(?:-*[^->])*--+|-?)'
+        // Special "raw text" elements whose content should be elided.
+        + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*' + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+        // Regular name
+        + '|/?[a-z]' + tagBody + ')>',
+        'gi');
+
+    var oldHtml;
+    do {
+        oldHtml = html;
+        html = html.replace(tagOrComment, '');
+    } while (html !== oldHtml);
+    return html.replace(/</g, '&lt;');
+}
+console.log(a);
+console.log(removeTags(a));
+console.log(b);
+console.log(removeTags(b));
